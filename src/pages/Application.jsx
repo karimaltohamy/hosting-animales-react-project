46a9ac603bbd15d3d0246ca86bda10975fd06ded
Mohamed gamal <mohamed555asdfgh@gmail.com>
2024-07-05 16:30:14 +0300

    build

--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
 const Application = () => {
   return (
@@ -9,7 +10,7 @@ const Application = () => {
       <Header />
       <Outlet />
       {/* <Navbar /> */}
-
+      <Footer/>
     </Fragment>
   );
 };
