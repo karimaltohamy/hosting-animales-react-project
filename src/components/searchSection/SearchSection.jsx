a3b9f7292dd3b1761706ee3ba5b57f12663aa9c3
karimaltohamy <karimbadr2003@gmail.com>
2024-07-07 20:40:41 +0200

    handle sections on web

--- src/components/searchSection/SearchSection.jsx
+++ src/components/searchSection/SearchSection.jsx
@@ -9,6 +9,8 @@ import { MdOutlineHotel } from "react-icons/md";
 import { FaPerson } from "react-icons/fa6";
 import { CgGym } from "react-icons/cg";
 import { RiPsychotherapyLine } from "react-icons/ri";
+import { format } from "date-fns";
+import { MdOutlinePets } from "react-icons/md";
 
 const SearchSection = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +18,6 @@ const SearchSection = () => {
   const [endDate, setEndDate] = useState(null);
 
   const onChange = (dates) => {
-    console.log(dates);
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
@@ -25,56 +26,68 @@ const SearchSection = () => {
       setIsOpen(false);
     }
   };
+
   return (
     <div className="container">
-          <div className="mt-4  rounded-lg border-2 border-primary search_section">
-      <div className="categories flex items-center justify-center flex-wrap gap-1">
-        <div className="item active">
-          <MdOutlineHotel size={18} />
-          <span>Hotel</span>
-        </div>
-        <div className="item">
-          <FaPerson size={18} />
-          <span>Persons</span>
-        </div>
-        <div className="item">
-          <CgGym size={18} />
-          <span>Exercises</span>
-        </div>
-        <div className="item">
-          <RiPsychotherapyLine size={18} />
-          <span>Treatment</span>
-        </div>
-      </div>
-      <div className="input_item">
-        <IoLocationOutline size={30} />
-        <input type="text" placeholder="Write The Location ?" />
-      </div>
-      <div className="input_item">
-        <WiDaySunnyOvercast size={30} />
-        <input type="number" placeholder="Write The Number Of Days ?" />
-      </div>
-      <div className="input_item">
-        <BsCalendar2Date onClick={() => setIsOpen(!isOpen)} size={30} />
-        <span onClick={() => setIsOpen(!isOpen)} className="text-gray-400">
-          Write The Date ?
-        </span>
-        {isOpen && (
-          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 z-20">
-            <DatePicker
-              selected={startDate}
-              onChange={onChange}
-              startDate={startDate}
-              endDate={endDate}
-              selectsRange
-              selectsDisabledDaysInRange
-              inline
-            />
+      <div className="mt-4  rounded-lg border-2 border-primary search_section">
+        <div className="categories flex items-center justify-center flex-wrap gap-1">
+          <div className="item active">
+            <MdOutlineHotel size={18} />
+            <span>Hotel</span>
           </div>
-        )}
+          <div className="item">
+            <FaPerson size={18} />
+            <span>Persons</span>
+          </div>
+          <div className="item">
+            <CgGym size={18} />
+            <span>Exercises</span>
+          </div>
+          <div className="item">
+            <RiPsychotherapyLine size={18} />
+            <span>Treatment</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 flex-col md:flex-row">
+          <div className="input_item">
+            <IoLocationOutline size={30} />
+            <input type="text" placeholder="Write The Location ?" />
+          </div>
+          <div className="input_item">
+            <WiDaySunnyOvercast size={30} />
+            <input type="number" placeholder="Write The Number Of Days ?" />
+          </div>
+          <div className="input_item">
+            <MdOutlinePets size={27} />
+            <input type="number" placeholder="Write The Number Of Pets ?" />
+          </div>
+          <div className="input_item relative">
+            <BsCalendar2Date onClick={() => setIsOpen(!isOpen)} size={27} />
+            <span onClick={() => setIsOpen(!isOpen)} className="text-gray-400">
+              {startDate && endDate
+                ? `${format(startDate, "yyyy-MM-dd")} - ${format(
+                    endDate,
+                    "yyyy-MM-dd"
+                  )}`
+                : " Write The Date ?"}
+            </span>
+            {isOpen && (
+              <div className="absolute top-10 left-1/2 -translate-x-1/2 z-20">
+                <DatePicker
+                  selected={startDate}
+                  onChange={onChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  selectsRange
+                  selectsDisabledDaysInRange
+                  inline
+                />
+              </div>
+            )}
+          </div>
+          <button className="btn_submit">Search</button>
+        </div>
       </div>
-      <button className="btn_submit">Search</button>
-    </div>
     </div>
   );
 };
